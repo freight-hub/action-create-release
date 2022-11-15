27ba9aed0d40ded7a9e134713a8bb188f9700f0b
Lennart Querter <lennart.querter@gmail.com>
2022-11-15 09:36:14 +0100

    get tag

--- dist/index.js
+++ dist/index.js
@@ -9689,11 +9689,12 @@ const github = __nccwpck_require__(8633);
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
+    const nameToGreet = core.getInput('version');
+
+    const tags = github.rest.git.getTag()
+    console.log(tags)
+
     console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 } catch (error) {
